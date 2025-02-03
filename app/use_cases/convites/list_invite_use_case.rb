20ff8f322589079ccfe49fcea7a5be51feb7ab2d
Daniel-mineirinho <daniel.candido@trackmob.com.br>
2025-02-03 00:44:20 -0300

    refatoração no filtro

--- app/use_cases/convites/list_invite_use_case.rb
+++ app/use_cases/convites/list_invite_use_case.rb
@@ -9,19 +9,25 @@ module UseCases
         convites = Convite.all
 
         convites = convites.where('email LIKE ?', "%#{@filters[:email]}%") if @filters[:email].present?
-
         convites = convites.where(empresa_id: @filters[:empresa_id]) if @filters[:empresa_id].present?
-
-        if @filters[:status].present?
-          convites = convites.where(status: @filters[:status])
-        end
+        convites = convites.where(status: @filters[:status]) if @filters[:status].present?
 
         if @filters[:start_date].present? && @filters[:end_date].present?
-          convites = convites.where(data_envio: @filters[:start_date]..@filters[:end_date])
+          convites = convites.where(
+            '(data_envio BETWEEN ? AND ?) OR (status = ? AND updated_at BETWEEN ? AND ?)',
+            @filters[:start_date], @filters[:end_date],
+            'Ativo', @filters[:start_date], @filters[:end_date]
+          )
         elsif @filters[:start_date].present?
-          convites = convites.where('data_envio >= ?', @filters[:start_date])
+          convites = convites.where(
+            'data_envio >= ? OR (status = ? AND updated_at >= ?)',
+            @filters[:start_date], 'Ativo', @filters[:start_date]
+          )
         elsif @filters[:end_date].present?
-          convites = convites.where('data_envio <= ?', @filters[:end_date])
+          convites = convites.where(
+            'data_envio <= ? OR (status = ? AND updated_at <= ?)',
+            @filters[:end_date], 'Ativo', @filters[:end_date]
+          )
         end
 
         convites
